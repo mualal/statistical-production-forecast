e746a67ccc8ba12f8fce8160bb3bc96471ba9b6b
Alexander Muravtsev <almuravcev@yandex.ru>
2022-10-14 21:57:37 +0300

    add file dialog window

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import sys
-
+import os
+import pandas as pd
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QGridLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QGridLayout, QLabel, QFileDialog
+from helpful_tools import history_preprocessing
 
+
+def choose_file_with_monthly_operating_report():
+    file_path, check = QFileDialog.getOpenFileName(
+        parent=None,
+        caption='Выберите файл с МЭР',
+        directory=os.path.dirname(os.getcwd()).replace(os.sep, '/'),
+        filter='Excel files (*.xlsx)'
+    )
+    if check:
+        df_initial = pd.read_excel(file_path, sheet_name='МЭР')
+        df_initial = history_preprocessing(df_initial, 365)
+        print(df_initial)
+    
 
 class MainWindow(QWidget):
     def __init__(
@@ -11,8 +26,12 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle('Статистика')
+
         calculate_button = QPushButton('Рассчитать')
+        calculate_button.clicked.connect(choose_file_with_monthly_operating_report)
+
         download_button = QPushButton('Загрузить')
+
         tool_label = QLabel(
             'Инструмент для прогнозирования показателей \nбазовой добычи нефти'
             ' и обводнённости на основе \nмесячного эксплуатационного рапорта (МЭР)'
